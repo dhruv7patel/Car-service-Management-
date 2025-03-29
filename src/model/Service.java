a547aa6ca22c196da7c7765a7d0cfcfdd9b86452
Vedant090804 <veprajapati@algomau.ca>
2025-03-29 01:02:00 -0400

    Update Service.java

--- src/model/Service.java
+++ src/model/Service.java
@@ -1,1 +1,64 @@
+package model;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
+public class Service {
+    private int serviceId;
+    private String name;
+    private String description;
+    private double price;
+    private final BooleanProperty selected = new SimpleBooleanProperty();
+
+    public Service(int serviceId, String name, String description, double price) {
+        this.serviceId = serviceId;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(int serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isSelected() {
+        return selected.get();
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected.set(selected);
+    }
+
+    public BooleanProperty selectedProperty() {
+        return selected;
+    }
+}
 
