3e5696609e7c9f78d68b952dd5ed293fbcd13095
Dhyaan04 <dhyaanpatel04@gmail.com>
2025-03-30 15:06:35 -0400

    Update AdminController.java

--- src/controllers/AdminController.java
+++ src/controllers/AdminController.java
@@ -1,1 +1,24 @@
+package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AdminController {
+    @FXML
+    private Button logoutBtn;
+    @FXML
+    private void handleLogout() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+            Stage stage = (Stage) logoutBtn.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Car Service Management System");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
