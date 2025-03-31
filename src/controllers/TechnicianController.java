d859094206189e423a3f10efa12cec9a751c501f
vishu2709 <patelvaishvi027@gmail.com>
2025-03-31 08:41:04 -0400

    Update TechnicianController.java

--- src/controllers/TechnicianController.java
+++ src/controllers/TechnicianController.java
@@ -1,1 +1,24 @@
+package controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TechnicianController {
+    @FXML
+    private Button logoutBtn;
+    @FXML
+    private void handleLogout() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+            Stage stage = (Stage) logoutBtn.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Car Service Management System");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
