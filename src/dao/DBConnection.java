52a5b42c8366f30693c9d32a17c565e88a37f97c
Dhruv <dhruvmpatel13@gmail.com>
2025-04-03 10:06:58 -0400

    Update DBConnection.java

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -1,1 +1,20 @@
+package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/car_service_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Dhruv@1373"; // Your MySQL password here
+    
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver not found", e);
+        }
+    }
+}
